e10618aac55904ac2498b1610fcc00cc466aa386
Dieter T <dieter.trappiel@protonmail.com>
2018-05-09 20:08:29 +1000

    Added .config folder

---
+++ .config/i3/monitor-hdmi.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#xrandr --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output LVDS1 --off --output VIRTUAL1 --off --output DP1 --off --output VGA1 --off
+xrandr --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output LVDS-1 --off --output DP-1 --off --output VGA-1 --off
---
+++ .config/i3/monitor-lcd.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#xrandr --output HDMI1 --off --output LVDS1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output VIRTUAL1 --off --output DP1 --off --output VGA1 --off
+xrandr --output HDMI-1 --off --output LVDS-1 --primary --mode 1366x768 --pos 0x0 --rotate normal --output DP-1 --off --output VGA-1 --off
